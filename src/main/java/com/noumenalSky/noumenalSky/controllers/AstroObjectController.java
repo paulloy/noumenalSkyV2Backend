ac9f01c75d061efe1621b289fff3073c35e46f45
Paul Loy <ploy@highroads.com>
2022-07-20 21:53:11 +0100

    Get all astroObjects by type

--- src/main/java/com/noumenalSky/noumenalSky/controllers/AstroObjectController.java
+++ src/main/java/com/noumenalSky/noumenalSky/controllers/AstroObjectController.java
@@ -2,6 +2,7 @@ package com.noumenalSky.noumenalSky.controllers;
 
 import com.noumenalSky.noumenalSky.models.AstroObject;
 import com.noumenalSky.noumenalSky.repositories.AstroObjectRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,20 +14,28 @@ import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/objects")
 public class AstroObjectController {
 
     @Autowired
     private AstroObjectRepository astroObjectRepository;
 
-    @GetMapping("/objects")
+    @GetMapping()
     public ResponseEntity<List<AstroObject>> getAllAstroObjects() {
         List<AstroObject> astroObjects = astroObjectRepository.findAll();
 
         return new ResponseEntity<>(astroObjects, HttpStatus.OK);
     }
 
-    @PostMapping("/objects")
+    @GetMapping("/{type}")
+    public ResponseEntity<List<AstroObject>> getAstroObjectsByType(
+            @PathVariable("type") String type
+    ) {
+        List<AstroObject> astroObjects = astroObjectRepository.findByType(type);
+        return new ResponseEntity<>(astroObjects, HttpStatus.OK);
+    }
+
+    @PostMapping()
     public ResponseEntity<AstroObject> createObject(@RequestBody AstroObject object) {
         AstroObject astroObject = astroObjectRepository.save(object);
         return new ResponseEntity<>(astroObject, HttpStatus.OK);
